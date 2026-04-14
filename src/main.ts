1cddde055aa04a735fe913a0bc4e98bf93a5776f
Raihan-Ardhanis <raihan_raharja_33rpl@student.smktelkom-mlg.sch.id>
2026-04-14 14:32:06 +0700

    fix: update start script for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Kos Hunter API')
-    .setDescription('API UKK RPL')
+    .setDescription('API Sistem Kos')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
-
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
